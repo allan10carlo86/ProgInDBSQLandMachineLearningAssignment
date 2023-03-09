ff0a495550faaf1e366e44ebb9d6e360c44e3ee4
Allan Carlo Ramos <allan.carlo.t.ramos@gmail.com>
2023-03-09 21:29:52 +0800

    Update Assignment Mar 9 2023 - Allan Carlo T. Ramos

--- SQL_Allan_Carlo/Assignment on SQL and Machine Learning.sql
+++ SQL_Allan_Carlo/Assignment on SQL and Machine Learning.sql
@@ -4,11 +4,12 @@
  Date Created: March 7, 2023
  Log Changes:
  -  March 7 2023   | Allan Carlo T. Ramos |    Initial Draft
+ -  March 9 2023.  | Allan Carlo T. Ramos |    Updated SQL to include UNION statement
  */
 
 use fillians_toy_shop;
 
-SELECT total_completed_orders_2003_table.cust_no,
+SELECT total_completed_orders_2003_table.cust_name,
        total_completed_orders_2003_table.order_no_count,
        total_completed_orders_2004_table.order_no_count,
        total_completed_orders_2005_table.order_no_count
@@ -48,9 +49,11 @@ FROM (SELECT customers.customer_no AS cust_no,
         AND orders.status = "Shipped"
       GROUP BY customers.customer_no
       ORDER BY customers.customer_no ASC) total_completed_orders_2005_table
-     ON total_completed_orders_2003_table.cust_no = total_completed_orders_2005_table.cust_no;
-
-SELECT total_completed_orders_2004_table.cust_no,
+     ON total_completed_orders_2003_table.cust_no = total_completed_orders_2005_table.cust_no
+WHERE NOT (total_completed_orders_2004_table.order_no_count IS NULL
+  AND total_completed_orders_2005_table.order_no_count IS NULL)
+UNION
+SELECT total_completed_orders_2004_table.cust_name,
        total_completed_orders_2003_table.order_no_count,
        total_completed_orders_2004_table.order_no_count,
        total_completed_orders_2005_table.order_no_count
@@ -90,7 +93,10 @@ FROM (SELECT customers.customer_no AS cust_no,
         AND orders.status = "Shipped"
       GROUP BY customers.customer_no
       ORDER BY customers.customer_no ASC) total_completed_orders_2005_table
-     ON total_completed_orders_2004_table.cust_no = total_completed_orders_2005_table.cust_no;
+     ON total_completed_orders_2004_table.cust_no = total_completed_orders_2005_table.cust_no
+WHERE total_completed_orders_2003_table.order_no_count IS NULL AND NOT total_completed_orders_2005_table.order_no_count IS NULL ;
+
+
 
 
 
